16f260eac5385e2e3cd81a59aeb062864a1e3b3f
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2022-10-10 23:09:22 +0530

    Adding React To existing HTML

---
+++ like_button.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+        
+
+    //   jsx
+    // <button onClick={() => this.setState({ liked: true })}>
+    //       Like
+    //    </button>
+    
+    // ABOVE JSX CODE WAS THROWING ME SOME ERRORS 
+
+    );
+  }
+}
+
+    const domContainer = document.querySelector('#like_button_container');
+    const root = ReactDOM.createRoot(domContainer);
+    root.render(e(LikeButton));
